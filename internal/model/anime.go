3638da3791c41752cd848e71c1cb5847272362ca
15708440182 <717449318@qq.com>
2026-06-30 18:27:52 +0800

    feat(model): Anime 新增 LastNotifiedEpisode 字段（任务 3/6）

--- internal/model/anime.go
+++ internal/model/anime.go
@@ -14,6 +14,7 @@ type Anime struct {
 	Cover               string    `json:"cover"`
 	CurrentRemarks      string    `json:"current_remarks"`
 	LastNotifiedRemarks string    `json:"last_notified_remarks"`
+	LastNotifiedEpisode int       `json:"last_notified_episode"`
 	CreatedAt           time.Time `json:"created_at"`
 }
 
